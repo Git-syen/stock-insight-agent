a1ff0756332b1205384f41909721c5ccfb350caa
Git-syen <srinivasaryenugu@gmail.com>
2025-06-30 18:07:32 +0000

    🔁 Updated rs stock filter

--- filters/rs.py
+++ filters/rs.py
@@ -10,8 +10,10 @@ def run_rs_filter(df: pd.DataFrame, index_df: pd.DataFrame, rs_period: int = 252
 
     # Calculate RS and RS Ratio
     #df["RS"] = df["Close"] / index_df["Close"]
-    df = df.merge(index_df[["Timestamp", "Close"]].rename(columns={"Close": "Benchmark_Close"}), on="Timestamp", how="left")
-    df["RS"] = 100 * (1+(df["Close"] / df["Benchmark_Close"]-1))/(1+(df["Close"].shift(rs_period) / df["Benchmark_Close"].shift(rs_period)-1))
+    #df = df.merge(index_df[["Timestamp", "Close"]].rename(columns={"Close": "Benchmark_Close"}), on="Timestamp", how="left")
+    #df["RS"] = 100 * (1+(df["Close"] / df["Benchmark_Close"]-1))/(1+(df["Close"].shift(rs_period) / df["Benchmark_Close"].shift(rs_period)-1))
+
+    df["RS"] = 100 * (1+(df["Close"] / index_df["Close"]-1))/(1+(df["Close"].shift(rs_period) / index_df["Close"].shift(rs_period)-1))
 
 
     # Filter strong RS symbols
